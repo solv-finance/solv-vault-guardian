9b4b5c4a53806bb11218af7f258f9d0d13960236
Ethan Y. Tsai <yee.tsai@gmail.com>
2024-03-04 14:59:07 +0800

    fix audit issue: SOLV-4

--- src/SolvVaultGuardianForSafe13.sol
+++ src/SolvVaultGuardianForSafe13.sol
@@ -6,9 +6,15 @@ import {Guard, Enum} from "safe-contracts-1.3.0/base/GuardManager.sol";
 import {SolvVaultGuardianBase} from "./common/SolvVaultGuardianBase.sol";
 
 contract SolvVaultGuardianForSafe13 is Guard, SolvVaultGuardianBase {
+    bool public allowEnableModule;
+
     constructor(address safeAccount_, address safeMultiSend_, address governor_, bool allowSetGuard_)
         SolvVaultGuardianBase(safeAccount_, safeMultiSend_, governor_, allowSetGuard_)
     {}
+
+    function enableModule(bool enable) external onlyGovernor {
+        allowEnableModule = enable;
+    }
 
     function checkTransaction(
         address to,
@@ -23,6 +29,11 @@ contract SolvVaultGuardianForSafe13 is Guard, SolvVaultGuardianBase {
         bytes memory, /*signatures*/
         address msgSender
     ) external virtual override {
+        //check safe account enableModule
+        if (to == safeAccount && data.length >= 4 && bytes4(data[0:4]) == bytes4(keccak256("enableModule(address)"))) {
+            require(allowEnableModule, "SolvVaultGuardian: enableModule disabled");
+            return;
+        }
         _checkSafeTransaction(to, value, data, msgSender);
     }
 
