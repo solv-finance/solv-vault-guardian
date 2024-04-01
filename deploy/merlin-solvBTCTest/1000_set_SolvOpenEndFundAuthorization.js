baff78cdea91af13feb2ecded8c69363b8557e53
Ethan Y. Tsai <yee.tsai@gmail.com>
2024-04-01 19:14:45 +1100

    support merlin vault

--- deploy/merlin-solvBTCTest/1000_set_SolvOpenEndFundAuthorization.js
+++ deploy/merlin-solvBTCTest/1000_set_SolvOpenEndFundAuthorization.js
@@ -9,13 +9,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deployer } = await getNamedAccounts();
   const signer = await getSigner(deployer);
 
-  const safeGovernor = "";
+  const safeGovernor = "0xE8E7764EDe76bB7306C21BaD07D6da2c30B81b8F";
   const guardianAddress = (
     await deployments.get("merlin-solvBTCTest-SolvVaultGuardianForSafe13")
   ).address;
 
-  const openEndFundShare = "";
-  const openEndFundRedemption = "";
+  const openEndFundShare = "0x1f4d23513c3ef0d63b97bbd2ce7c845ebb1cf1ce";
+  const openEndFundRedemption = "0x9d7795c2bd2ef6098c74cb0865691cbf6aa40887";
   const openEndFundAuthorizationAddress = (
     await deployments.get("merlin-solvBTCTest-SolvOpenEndFundAuthorization")
   ).address;
