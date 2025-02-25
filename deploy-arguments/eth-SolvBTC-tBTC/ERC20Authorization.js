2701dbd927a536e43e8040cd933c8ece7f67e0dc
Ethan Y. Tsai <yee.tsai@gmail.com>
2025-02-25 19:42:54 +1100

    deploy authorization for SolvBTC(tBTC,WBTC) on mainnet

--- deploy-arguments/eth-SolvBTC-tBTC/ERC20Authorization.js
+++ deploy-arguments/eth-SolvBTC-tBTC/ERC20Authorization.js
@@ -2,9 +2,9 @@ module.exports = [
   "0x6859A552b918dffbD29b9A446061249d72c872fa",
   [
     [
-      "0x18084fbA666a33d37592fA2633fD49a74DD93a88", // BTCB
+      "0x18084fbA666a33d37592fA2633fD49a74DD93a88", // tBTC
       [
-        "0xb816018e5d421e8b809a4dc01af179d86056ebdf", // OpenEndFundShare
+        "0x7d6C3860B71CF82e2e1E8d5D104CF77f5B84f93a", // OpenEndFundShare
         "0x1D0Db695F3033875d1b6A0155c38B3EE2AEd3082", // OpenEndFundRedemption
       ],
     ],
--- deploy-arguments/eth-SolvBTC-tBTC/SolvOpenEndFundAuthorization.js
+++ deploy-arguments/eth-SolvBTC-tBTC/SolvOpenEndFundAuthorization.js
@@ -1,10 +1,8 @@
 module.exports = [
   "0x6859A552b918dffbD29b9A446061249d72c872fa",
-  "0xb816018e5d421e8b809a4dc01af179d86056ebdf",
+  "0x7d6C3860B71CF82e2e1E8d5D104CF77f5B84f93a",
   "0x1D0Db695F3033875d1b6A0155c38B3EE2AEd3082",
-  [
-    "0x23299b545056e9846725f89513e5d7f65a5034ab36515287ff8a27e860b1be75",
-  ]
-]
+  ["0x23299b545056e9846725f89513e5d7f65a5034ab36515287ff8a27e860b1be75"],
+];
 
 // npx hardhat verify --network mainnet 0x359C926a877391defdDC0cCd0357D05666A8841d --constructor-args deploy-arguments/eth-solvBTC-tBTC/SolvOpenEndFundAuthorization.js
