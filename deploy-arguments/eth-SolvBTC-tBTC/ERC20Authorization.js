e1a543a4a057efed1538c93dc1d699976c3d8ea1
AlvisDu <dujc2020@gmail.com>
2025-02-25 15:52:42 +0800

    verify

---
+++ deploy-arguments/eth-SolvBTC-tBTC/ERC20Authorization.js
@@ -0,0 +1,15 @@
+module.exports = [
+  "0x6859A552b918dffbD29b9A446061249d72c872fa",
+  [
+    [
+      "0x18084fbA666a33d37592fA2633fD49a74DD93a88", // BTCB
+      [
+        "0xb816018e5d421e8b809a4dc01af179d86056ebdf", // OpenEndFundShare
+        "0x1D0Db695F3033875d1b6A0155c38B3EE2AEd3082", // OpenEndFundRedemption
+      ],
+    ],
+  ],
+  [],
+];
+
+// npx hardhat verify --network mainnet 0x94D0B43Aef0A6208dD857C1cb66F94abB314c3cB --constructor-args deploy-arguments/eth-solvBTC-tBTC/ERC20Authorization.js
